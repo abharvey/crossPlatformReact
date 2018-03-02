18a1bbf8e6a5d5e258c09e5244bd55d38755d83e
abharvey <aharvey@prontoforms.com>
2018-03-02 09:33:08 -0500

    react-primitives working with web build

--- web/webpack/webpack.dev.config.js
+++ web/webpack/webpack.dev.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
-  entry: ['webpack-hot-middleware/client', 'babel-polyfill', path.join(__dirname, '../../app/web/index')],
+  entry: ['webpack-hot-middleware/client', 'babel-polyfill', path.join(__dirname, '../../app/web/index.web')],
   output: {
     path: path.join(__dirname, '../public'),
     filename: 'bundle.js',
